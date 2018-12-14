f8ca08209436caff0880e227348061442573d00a
Liow Jen Nick <jennick@compass-interactive.com>
2018-12-14 11:04:51 +0800

    Added difficulty.

--- blockchain.js
+++ blockchain.js
@@ -1,13 +1,29 @@
-const uuidv1 = require('uuid/v1');
+const crypto = require('crypto')
 
 class Blockchains {
     constructor () {
         this.blocks = []
     }
 
+    createUUID () {
+        const dt = new Date().getMilliseconds()
+        const uuid = ((dt + Math.random() * 16) % 16 | 0).toString
+        return uuid
+    }
+
     addBlock (user, amount) {
-        let newBlock = new Block(user, amount)
-        
+        const newBlock = new Block(user, amount)
+        const cipherHash = crypto.createHash('sha1')
+                                .update(this.createUUID() + JSON.stringify(newBlock.data))
+                                .digest('hex')
+
+        newBlock.hash = cipherHash
+
+        // increase difficulty for every 5 blocks
+        newBlock.difficulty = Math.round(newBlock.difficulty + (this.blocks.length / 5))
+
+        this.findSolution(newBlock)
+
         if (this.blocks.length === 0) {
             newBlock.id = null
         } else {
@@ -17,10 +33,42 @@ class Blockchains {
         this.blocks.push(newBlock)
     }
 
+    findSolution (block) {
+        // number of loop
+        let nonce = 0
+
+        while (true) {
+            let answer = crypto.createHash('sha256')
+                            .update(this.createUUID() + JSON.stringify(block.data) + nonce)
+                            .digest('hex')
+
+            // if (nonce % 5000 === 0) {
+            //     console.log(nonce)
+            //     console.log(answer)
+            //     console.log(block.hash)
+            // }
+
+            if (answer.startsWith('0'.repeat(block.difficulty))) {
+                console.log(block.hash)                
+                console.log('Success')
+                
+                block.answer = answer
+                block.nonce = nonce
+                return
+            }
+
+            nonce++
+        }
+    }
+
+    verifySolution () {
+
+    }
+
     verifyChain () {
         for (var i = 0; i < this.blocks.length; i++) {
             if (i === (this.blocks.length - 1)) {
-                console.log("All blocks are verified.")
+                console.log("Last block is verified.")
             } else if (this.blocks[i].hash === this.blocks[i + 1].id) {
                 console.log(`Block ${i} is verfied`)
             } else {
@@ -33,7 +81,11 @@ class Blockchains {
 class Block {
     constructor (user, amount) {
         this.id = null,
-        this.hash = uuidv1(),
+        this.hash = null,
+        this.difficulty = 1,
+        this.nonce = 0,
+        this.answer = null,
+        this.result = null
         this.data = {
             user,
             amount
--- playground.js
+++ playground.js
@@ -1,11 +1,14 @@
 const Blockchains = require('./blockchain.js')
 
-let myBC = new Blockchains()
+const myBC = new Blockchains()
 
 // addBlock takes 2 arguments
-myBC.addBlock("Nick", 1000)
-myBC.addBlock("Bryan", 2000)
-myBC.addBlock("Sarah", 3000)
-
 // checks hash of previous block to current block id
+
 myBC.verifyChain()
+
+for (let i = 0; i < 10; i++) {
+    myBC.addBlock('Nick', 1000 * i)
+}
+
+console.log(myBC.blocks)
